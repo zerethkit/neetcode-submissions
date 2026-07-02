90afc111a33a785f9faf06b09224e2eef71430f7
zerethkit <zerethkit@gmail.com>
2026-07-03 03:50:02 +0800

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashSet.contains(nums[i])) {
+                return true;
+            }
+            hashSet.add(nums[i]);
+        }
+        return false;
+    }
+}
